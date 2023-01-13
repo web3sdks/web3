4c533ee216daf9eeb96af1fc49bcd37667b531bb
web3sdkio <demo@web3sdks.com>
2023-01-13 15:30:19 +0800

    Fix

--- packages/sdk/src/evm/constants/addresses.ts
+++ packages/sdk/src/evm/constants/addresses.ts
@@ -28,11 +28,11 @@ export const CONTRACT_ADDRESSES: Record<
 > = {
   [ChainId.Mainnet]: {
     openzeppelinForwarder: OZ_DEFENDER_FORWARDER_ADDRESS,
-    openzeppelinForwarderEOA: "0x29ce93BAD941e89b4661121195275C1132e777FE",
-    biconomyForwarder: "0x170F1B2E15262860E65296A213Bff22f2378E189",
-    twFactory: TWFactory_address,
-    twRegistry: TWRegistry_address,
-    twBYOCRegistry: "0x9762ec95fc18FCbAe018d8f99D81FDBDE34019EF",
+    openzeppelinForwarderEOA: "0x0000000000000000000000000000000000000000",
+    biconomyForwarder: "0x0000000000000000000000000000000000000000",
+    twFactory: "0xD9CEE8cB329Ea726F92A87Dc118A39Db3b93250B",
+    twRegistry: "0x5fd766aD7E861d12D3aBd6428fF19E363BDB8b7b",
+    twBYOCRegistry: "0x0000000000000000000000000000000000000000",
   },
 
   [ChainId.Goerli]: {
@@ -95,8 +95,8 @@ export const CONTRACT_ADDRESSES: Record<
     openzeppelinForwarder: OZ_DEFENDER_FORWARDER_ADDRESS,
     openzeppelinForwarderEOA: "0x29ce93BAD941e89b4661121195275C1132e777FE",
     biconomyForwarder: "0x170F1B2E15262860E65296A213Bff22f2378E189",
-    twFactory: TWFactory_address,
-    twRegistry: TWRegistry_address,
+    twFactory: "0xC611709B38ab817f2201D6bb8C64D6DbF76EEe18",
+    twRegistry: "0x2b115B27cD07385BfeC7E1b9483C9520Ec625E65",
     twBYOCRegistry: "0x9762ec95fc18FCbAe018d8f99D81FDBDE34019EF",
   },
 
@@ -152,10 +152,10 @@ export const APPROVED_IMPLEMENTATIONS: Record<
   Record<DropContract, string>
 > = {
   [ChainId.Mainnet]: {
-    "nft-drop": "0xC611709B38ab817f2201D6bb8C64D6DbF76EEe18",
-    "edition-drop": "0xBF2d2c61a52a0Bb19626C6D00113672C566DfD65",
-    "token-drop": "0x7d2dc0109C4a0251A626c3F728b7cC14407665F2",
-    "signature-drop": "0xD9CEE8cB329Ea726F92A87Dc118A39Db3b93250B",
+    "nft-drop": "0xF2CF34a9928a6aB04881bfEaAC5BedB6d05Abc27",
+    "edition-drop": "0xbc3D10e91D5fE510f6Fa8C8c4e285B9811BC928c",
+    "token-drop": "0x5e1c754f28b7E8458Ec4E440F9187856bfeb048c",
+    "signature-drop": "0x0000000000000000000000000000000000000000",
   },
   [ChainId.Polygon]: {
     "nft-drop": "0x106a6080ef4889458D25f4404a88f20B5b30f560",
@@ -182,7 +182,7 @@ export const APPROVED_IMPLEMENTATIONS: Record<
     "signature-drop": "0xD9CEE8cB329Ea726F92A87Dc118A39Db3b93250B",
   },
   [ChainId.Arbitrum]: {
-    "nft-drop": "0xC611709B38ab817f2201D6bb8C64D6DbF76EEe18",
+    "nft-drop": "0x3B32f5132EE1BaC2CBbb431C9A68D0841985c41b",
     "edition-drop": "0xBF2d2c61a52a0Bb19626C6D00113672C566DfD65",
     "token-drop": "0x7d2dc0109C4a0251A626c3F728b7cC14407665F2",
     "signature-drop": "0xD9CEE8cB329Ea726F92A87Dc118A39Db3b93250B",
